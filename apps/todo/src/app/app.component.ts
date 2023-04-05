ec2fbf2f89d841cc43afb24b3b5d846e1e26f8ec
John Coppola <jdcoppola@gmail.com>
2023-04-04 21:32:48 -0400

    FEATURE: injects HttpClientModule in app.component

--- apps/todo/src/app/app.component.ts
+++ apps/todo/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: "practice-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  imports: [NxWelcomeComponent, HttpClientModule, RouterModule],
+  selector: 'practice-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "todo";
+  title = 'todo';
 }
